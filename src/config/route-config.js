ffe54a3b8846c7c86936245b4a8eba849b73c6ad
Lindsay Koprivica <lindsayeplin@gmail.com>
2019-08-07 13:09:05 -0600

    checkpoint-deployment completed(set up project:postgres,express,heroku...etc)

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,6 +1,11 @@
 module.exports = {
     init(app){
+
       const staticRoutes = require("../routes/static");
+      const logger = require('morgan');
+
+
       app.use(staticRoutes);
+      app.use(logger('dev'));
     }
   }
